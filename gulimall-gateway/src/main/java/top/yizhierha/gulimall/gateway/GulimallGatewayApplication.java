3817554a3cc0608435206f34e6fd995f6e5d4c1d
Forever2ha <1539280617@qq.com>
2023-01-24 09:33:46 +0800

    合并之前写的代码

---
+++ gulimall-gateway/src/main/java/top/yizhierha/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,15 @@
+package top.yizhierha.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient //启动服务的注册和发现
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
